ed0e897a922e30a19975264c9b43d9022ca431ba
Aldo González <97136402+Scrollaside@users.noreply.github.com>
2025-05-20 17:56:29 -0600

    TESTING

--- pw2/front/src/AxiosConf/axiosconf.js
+++ pw2/front/src/AxiosConf/axiosconf.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3001',
-    baseURL: 'https://pw2-proyecto.onrender.com',
+    baseURL: 'back.pw2proyect.infinityfreeapp.com',
     withCredentials: true // importante para las cookies de sesión
 });
 
